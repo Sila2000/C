8b98ad343898c63187f1c2df9e27881c4225a2c8
Darwinian_Living_Machine2000 <118555211+Sila2000@users.noreply.github.com>
2023-11-01 01:58:49 +0530

    Add files via upload

---
+++ Playing_with_Array/deletion.c
@@ -0,0 +1,40 @@
+//Sila2000
+//Insertion
+
+#include<stdio.h>
+
+main()
+{
+    int size, i, num_list[100], post_del;
+
+    printf("Enter the size of vector: ");
+    scanf("%d", &size);
+
+    for(i = 0; i < size; i++)
+    {
+        printf("num[%d] = ", i);
+        scanf("%d", &num_list[i]);
+    }
+
+    printf("\nEnter the index of deletion: ");
+    scanf("%d", &post_del);
+
+    if(post_del < 0 || post_del > size)
+        printf("Index of deletion must be greater than or equal to 0 and less than or equal to %d\nThere is no DELETION.", size);
+    
+    else
+    {
+        printf("AFTER DELETION:\n");
+        
+        for(i = post_del; i < size; i++)
+        {
+            num_list[i] = num_list[i + 1];
+        }
+
+
+        for(i = 0; i < size - 1; i++)
+        {
+            printf("num[%d] = %d\n", i, num_list[i]);
+        }
+    }
+}
